f9a8bacb0977e6d66dd40b0c2e08fa1436027cc7
weizhen <1023453278@qq.com>
2020-11-19 09:35:15 +0800

    调拨支持打印

--- BizHandle/scripts/FSSC/GDAllocateFssc.js
+++ BizHandle/scripts/FSSC/GDAllocateFssc.js
@@ -236,10 +236,19 @@ window["invoker"].FSSC_fhback = function() {
 
 //打印
 window["invoker"].FSSC_print = function() {
-        return function() {
+        /*return function() {
             var def = $.Deferred();
             var selfinner = $('[data-view="Form1"]').data('viewInstance');
             return def.promise();
+        }*/
+        return function() {
+            var gdself = $('[data-view="Form1"]').data('viewInstance');
+            gdself.context.invoke([{
+                target: 'GDWebAllocationCardController',
+                methodName: 'PrintCard',
+                params: []
+            }]);
+            return $.Deferred().resolve([true]);
         }
     }
     ();
